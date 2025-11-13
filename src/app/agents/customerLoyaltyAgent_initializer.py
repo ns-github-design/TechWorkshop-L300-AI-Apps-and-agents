ce6dba29e03e391fab2fc57f4849436f9a3688a4
ns-github-design <61305121+ns-github-design@users.noreply.github.com>
2025-11-13 03:18:45 +0000

    updating code

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -12,7 +12,9 @@ load_dotenv()
 CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
 with open(CL_PROMPT_TARGET, 'r', encoding='utf-8') as file:
     CL_PROMPT = file.read()
-project_endpoint= os.getenv("AZURE_AI_AGENT_ENDPOINT")
+project_endpoint= os.getenv("AZURE_AI_AGENT_ENDPOINT") 
+agent_id = os.environ["customer_loyalty"]
+
 project_client = AIProjectClient(
     endpoint=project_endpoint,
     credential=DefaultAzureCredential(),
@@ -27,10 +29,27 @@ toolset.add(functions)
 project_client.agents.enable_auto_function_calls(tools=functions)
 
 with project_client:
-    agent = project_client.agents.create_agent(
-        model=os.getenv("AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"),  # Model deployment name
-        name="Zava Customer Loyalty Agent",  # Name of the agent
-        instructions=CL_PROMPT,  # Instructions for the agent
-        toolset=toolset,
-    )
-    print(f"Created agent, ID: {agent.id}")
+    agent_exists = False
+    if agent_id:
+        # Check if agent exists.
+        agent = project_client.agents.get_agent(agent_id)
+        print(f"Retrieved existing agent, ID: {agent.id}")
+        agent_exists = True
+    
+    if agent_exists:
+        agent = project_client.agents.update_agent(
+            agent_id=agent.id,
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name="Zava Customer Loyalty Agent",  # Name of the agent
+            instructions=CL_PROMPT,  # Updated instructions for the agent
+            # toolset=toolset
+        )
+        print(f"Updated agent, ID: {agent.id}")
+    else:
+        agent = project_client.agents.create_agent(
+            model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
+            name="Zava Customer Loyalty Agent",  # Name of the agent
+            instructions=CL_PROMPT,  # Instructions for the agent
+            # toolset=toolset
+        )
+        print(f"Created agent, ID: {agent.id}")
